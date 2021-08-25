2edd33039b8fab1000df00994be45b83c99cfaf8
Tom Hohl <tom.hohl@gmail.com>
2021-08-25 16:19:44 -0600

    item RD actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,27 @@
 class ItemsController < ApplicationController
+    before_action :set_dep
+    before_action :set_item, only: [:show, :destroy]
 
     def index
-        render component: "Items", props: {dep: @dep}
+        render component: "Items", props: {dep: @dep, items: @dep.items}
     end
+
+    def show
+    end
+
+    def destroy
+        @item.destroy
+        redirect_to department_items_path
+    end
+
 
     private
     def set_dep
-        @dep = Department.find(params[department_id])
+        @dep = Department.find(params[:department_id])
     end
+
+    def set_item
+        @item = @dep.items.find(params[:id])
+    end
+
 end
--- app/javascript/components/Items.js
+++ app/javascript/components/Items.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 const Items = (props) => {
+    const { dep, items } = props;
+
+    const renderItems = () => {
+        return items.map((item) => (
+            <div>
+                <h2>{`${item.name}`}</h2>
+                <a href={`/departments/${dep.id}/items/${item.id}`}>Show Item</a>
+                <a href={`/departments/${dep.id}/items/${item.id}`} data-method="delete">Delete Item</a>
+            </div>
+        ))
+    }
     return (
-        <h1>Items Test</h1>
+        <div>
+            <h1>{`${dep.name} items`}</h1>
+            <a href={`/departments/${dep.id}`}>Back to Department</a>
+            <a href={`/departments/${dep.id}/items/new`}>Add Item</a>
+            {renderItems()}
+        </div>
     )
 }
 
